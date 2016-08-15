c92ea6e1e8eb1bd6d986c5cd0d345e4d9e037c0a
lollypop27 <lara@adfactory.co.za>
2016-08-15 15:50:28 +0200

    setting up user authentication and signup

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,52 +1,67 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function ($rootScope,
+                                 $scope,
+                                 $ionicModal,
+                                 $timeout,
+                                 $auth,
+                                 $ionicLoading) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  $rootScope.$on('auth:login-success', function(ev, user) {
+    $scope.currentUser = user;
+  });
 
-  // Form data for the login modal
   $scope.loginData = {};
 
-  // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
     scope: $scope
   }).then(function(modal) {
     $scope.modal = modal;
   });
 
-  // Triggered in the login modal to close it
   $scope.closeLogin = function() {
     $scope.modal.hide();
   };
 
-  // Open the login modal
   $scope.login = function() {
     $scope.modal.show();
   };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
+  $scope.doLogin = function () {
+    $ionicLoading.show({
+     template: 'Logging in...'
+    });
+    $auth.submitLogin($scope.loginData)
+    .then(function (resp) {
+      // handle success response
+      $ionicLoading.hide();
       $scope.closeLogin();
-    }, 1000);
+    })
+    .catch(function (error) {
+      // handle error response
+      $ionicLoading.hide();
+      $scope.errorMessage = error;
+    });
+  };
+
+})
+.controller('SignupController', function($scope, $auth) {
+  $scope.handleRegBtnClick = function() {
+    $auth.submitRegistration($scope.registrationForm)
+      .then(function(resp) {
+        // handle success response
+      })
+      .catch(function(resp) {
+        // handle error response
+      });
   };
 })
 
 .controller('TestController', function($scope) {
-  $scope.gender = ['Male', 'Female']
+  $scope.gender = ['Male', 'Female'];
   $scope.ageValues = {
-    min: 20,
-    max: 60,
-    value: 20
+   min: 13,
+   max: 60,
+   value: 20
   };
   $scope.distanceValues = {
     min: 1000,
@@ -57,10 +72,11 @@ angular.module('starter.controllers', [])
   $scope.calculateCooper = function() {
     var person = new Person({
       gender: $scope.data.gender,
-      age: $scope.data.age
+      age: $scope.data.age,
+      distance: $scope.data.distance
     });
-    person.assessCooper($scope.data.distance);
+    person.cooper_result($scope.data);
     $scope.person = person;
-    console.log($scope.person)
+    console.log($scope.person);
   };
 });
